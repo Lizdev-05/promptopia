158f96f9c812d4f9cc7879198cf692644798a18c
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-08-02 18:33:44 +0100

    Create the logo and it is working well

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,23 @@
-import React from "react";
-
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import { signIn, signOut, getProvider } from "next-auth/react";
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="Promptopia logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className="logo_text">Promptopia</p>
+      </Link>
+    </nav>
+  );
 };
 
 export default Nav;
